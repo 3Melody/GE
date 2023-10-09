1be620a4a2fac044f65c5699d34abb2ce1afdde1
frosty-cell <65022353@up.ac.th>
2023-10-09 21:52:50 +0700

    fix some bugs

--- src/page5.js
+++ src/page5.js
@@ -6,16 +6,30 @@ function closePopup() {
     var popup = document.getElementById('popup');
     popup.style.display = 'none';
 }
+
+// เพิ่มตัวแปร global เพื่อเก็บค่า CSS display ของ div content
+var contentDisplayState = {}; // เราจะใช้ object ในการเก็บค่าเพื่อให้รองรับหลายรายวิชา
+
 // ฟังก์ชันซ่อนหรือแสดงรายวิชา
 function hideContent(contentId) {
     var content = document.getElementById(contentId);
-    if (content.style.display === 'none') {
-        content.style.display = 'block';
+    
+    // ตรวจสอบว่าเราเคยบันทึกค่า CSS display ไว้แล้วหรือยัง
+    if (typeof contentDisplayState[contentId] === 'undefined') {
+        // ถ้ายังไม่เคยบันทึกค่า CSS display ให้บันทึกค่าปัจจุบัน
+        contentDisplayState[contentId] = window.getComputedStyle(content).getPropertyValue('display');
+    }
+    
+    // เปลี่ยนค่า CSS display เพื่อแสดง div content หรือซ่อนไว้
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = contentDisplayState[contentId]; // เรียกคืนค่า CSS display
     } else {
         content.style.display = 'none';
     }
-    closePopup();
+    
+    closePopup(); // ปิด Popup หลังจากที่เลือกรายวิชา
 }
+
 function toggleDropdown() {
     var dropdown = document.getElementById("dropdown");
     if (dropdown.style.display === "none" || dropdown.style.display === "") {
@@ -29,6 +43,7 @@ function toggleDropdown() {
     }
 }
 
+// Playlist-order
 let draggedElement = null;
 
 function allowDrop(event) {
@@ -60,4 +75,3 @@ function drop(event) {
     }
     draggedElement = null;
 }
-
